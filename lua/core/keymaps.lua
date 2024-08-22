67f047eac50a673d8f8163163cf67e265d5c62b0
samodostal <samuel.dostal.sk@gmail.com>
2024-08-22 13:42:05 +0200

    Make window navigation faster

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -8,6 +8,11 @@ map("n", "<leader>t", ":b#<CR>")
 map("v", "<", "<gv")
 map("v", ">", ">gv")
 
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-l>", "<C-w>l")
+
 map_menu("Neovim management", "<leader>nn", {
 	{ text = "Check health", cmd = ":checkhealth" },
 	{ text = "Lazy", cmd = ":Lazy" },
