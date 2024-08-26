af76d5fa0851d304d4d6b818162eecc9f747dcdc
samodostal <samuel.dostal.sk@gmail.com>
2024-08-26 11:54:36 +0200

    Add nvim-notify for overseer notifications

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -52,4 +52,14 @@ return {
 			},
 		},
 	},
+	{
+		"rcarriga/nvim-notify",
+		config = function()
+			require("notify").setup {
+				render = "wrapped-compact",
+			}
+
+			vim.notify = require "notify"
+		end,
+	},
 }
