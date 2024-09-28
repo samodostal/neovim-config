ffb0cf4b1e2437df562e507095467092bd849477
samodostal <samuel.dostal.sk@gmail.com>
2024-09-28 11:13:41 +0200

    Add and modify common keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,12 +3,19 @@ local map_menu = require("core.utils").map_menu
 
 map("n", "<leader>", "<nop>")
 
+map("n", "<C-e>", "<C-i>")
+
+map("n", "<C-i>", "<C-u>")
+map("n", "<C-d>", "<C-d>")
+map("v", "<C-i>", "<C-u>")
+map("v", "<C-d>", "<C-d>")
+
 map("n", "<leader>t", ":b#<CR>")
 
 map("v", "<", "<gv")
 map("v", ">", ">gv")
 
-map("t", "<esc>", "<c-\\><c-n>")
+map("t", "<c-t>", "<c-\\><c-n>")
 
 map_menu("Neovim management", "<leader>nn", {
 	{ text = "Check health", cmd = ":checkhealth" },
