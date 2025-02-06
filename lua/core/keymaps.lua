066f2b2c969fcc0e1cf85d3f6b78697487ec0725
samodostal <samuel.dostal.sk@gmail.com>
2025-02-06 11:27:19 +0100

    Add a global mark keymap

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -10,17 +10,20 @@ map("n", "<C-d>", "<C-d>")
 map("v", "<C-i>", "<C-u>")
 map("v", "<C-d>", "<C-d>")
 
-map("n", "<leader>t", ":b#<CR>")
+map("n", "<leader>t", ":b#<cr>")
 
 map(
 	"n",
 	"gch",
-	":lua if vim.g.comment_color == '#989db5' then "
+	"<cmd>lua if vim.g.comment_color == '#989db5' then "
 		.. "vim.cmd('highlight Comment guifg=#565f89') "
 		.. "vim.g.comment_color = '#565f89' else "
 		.. "vim.cmd('highlight Comment guifg=#989db5') "
-		.. "vim.g.comment_color = '#989db5' end<CR>"
+		.. "vim.g.comment_color = '#989db5' end<cr>"
 )
+
+map("n", "gm", "mA")
+map("n", "gb", "'A")
 
 map("v", "<", "<gv")
 map("v", ">", ">gv")
@@ -28,10 +31,10 @@ map("v", ">", ">gv")
 map("t", "<c-t>", "<c-\\><c-n>")
 
 map_menu("Neovim management", "<leader>nn", {
-	{ text = "Check health", cmd = ":checkhealth" },
-	{ text = "Lazy", cmd = ":Lazy" },
-	{ text = "Mason", cmd = ":Mason" },
-	{ text = "LSP info", cmd = ":LspInfo" },
+	{ text = "Check health", cmd = "<cmd>checkhealth" },
+	{ text = "Lazy", cmd = "<cmd>Lazy" },
+	{ text = "Mason", cmd = "<cmd>Mason" },
+	{ text = "LSP info", cmd = "<cmd>LspInfo" },
 })
 
 map("n", "<leader>cc", "<cmd>lua require('core.compiler').compile_and_run()<cr>")
