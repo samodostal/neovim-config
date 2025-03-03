7398431c8cd54fe1916f94e35c3cb06d8defb956
samodostal <samuel.dostal.sk@gmail.com>
2025-03-03 18:18:41 +0100

    Change default oil.nvim keymaps

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -2,11 +2,28 @@ return {
 	{
 		"stevearc/oil.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-		opts = {
-			view_options = { show_hidden = true },
-		},
-		keys = {
-			{ "-", "<cmd>Oil<cr>", desc = "Open oil in current buffer" },
-		},
+		config = function()
+			require("oil").setup {
+				default_file_explorer = true,
+				view_options = { show_hidden = true },
+				use_default_keymaps = false,
+				keymaps = {
+					["g?"] = { "actions.show_help", mode = "n" },
+					["<CR>"] = "actions.select",
+					["<C-p>"] = "actions.preview",
+					["<C-c>"] = "actions.refresh",
+					["-"] = { "actions.parent", mode = "n" },
+					["_"] = { "actions.open_cwd", mode = "n" },
+					["`"] = { "actions.cd", mode = "n" },
+					["gs"] = { "actions.change_sort", mode = "n" },
+					["gx"] = "actions.open_external",
+					["g."] = { "actions.toggle_hidden", mode = "n" },
+				},
+			}
+
+			local map = require("core.utils").map
+
+			map("n", "-", "<cmd>Oil<cr>")
+		end,
 	},
 }
